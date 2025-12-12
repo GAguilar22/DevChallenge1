20dafd3a5e425322ac778a7e756a153d38a7127e
Gerard Aguilar <gerard.aguilar1@insbaixcamp.cat>
2025-12-12 16:07:43 +0100

    Prueba de subida

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'gaguilar22/devchallenge1',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'bd1151f21f04eab17da5bec039caa048c8f3edaf',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gaguilar22/devchallenge1' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'bd1151f21f04eab17da5bec039caa048c8f3edaf',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
